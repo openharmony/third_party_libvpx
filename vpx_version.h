d2a46ebeb001fa154698afb630aadc5c5aefd500
colinhl <helin48@huawei.com>
2025-12-26 16:04:56 +0800

    add OAT.xml
    Signed-off-by: colinhl <helin48@huawei.com>

--- vpx_version.h
+++ vpx_version.h
@@ -1,3 +1,15 @@
+/*
+ *  Copyright (c) 2025 The WebM project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+// This file is generated. Do not edit.
+
 #ifndef VPX_VERSION_H_
 #define VPX_VERSION_H_
 #define VERSION_MAJOR 1
