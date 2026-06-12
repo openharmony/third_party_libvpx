7bc033637ab5f967f4c0c2d6a4cd25265c72bd9a
ringking0 <renkang2@huawei.com>
2026-06-12 19:50:44 +0800

    #3234 libvpx: 添加x86_64架构支持
    Issue: https://gitcode.com/openharmony/startup_init/issues/3234
    
    ## 修改内容
    - 添加 target CPU 检测变量，区分 ARM、ARM64 和 X86_64。
    - 条件化 CONFIG_ARCH、NEON 和 ARM 浮点编译参数。
    - 为 x86_64 生成 RTCD 头文件。
    - 为 x86_64 添加 build.sh 配置和 RTCD 生成流程。
    - 修复 32-bit ARM 构建缺少 aarch32_cpudetect.c 导致 arm_cpu_caps 未定义的问题。
    
    Signed-off-by: ringking0 <renkang2@huawei.com>

--- build.sh
+++ build.sh
@@ -13,6 +13,8 @@ fi
 
 mkdir -p $OUTPUT_DIR
 rm -f "$OUTPUT_DIR/vpx_config.h"
+rm -f "$OUTPUT_DIR/vp8_rtcd.h" "$OUTPUT_DIR/vp9_rtcd.h" \
+  "$OUTPUT_DIR/vpx_dsp_rtcd.h" "$OUTPUT_DIR/vpx_scale_rtcd.h"
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
@@ -50,6 +52,56 @@ if [ "$CPU" == "arm" ]; then
       --disable-webm-io  \
       --disable-sve  \
       --disable-neon
+fi
+
+if [ "$CPU" == "x86_64" ]; then
+  ./configure \
+      --target=generic-gnu \
+      --enable-vp9-highbitdepth \
+      --disable-vp8-encoder \
+      --disable-vp9-encoder \
+      --disable-unit-tests \
+      --disable-examples \
+      --disable-tools \
+      --disable-docs \
+      --disable-pic \
+      --disable-libyuv \
+      --disable-webm-io \
+      --disable-runtime-cpu-detect
+
+  cat > rtcd_config.mk <<'EOF'
+CONFIG_RUNTIME_CPU_DETECT=no
+CONFIG_VP8_DECODER=yes
+CONFIG_VP8_ENCODER=no
+CONFIG_VP9=yes
+CONFIG_VP9_DECODER=yes
+CONFIG_VP9_ENCODER=no
+CONFIG_VP9_HIGHBITDEPTH=yes
+CONFIG_SPATIAL_RESAMPLING=yes
+CONFIG_POSTPROC=no
+CONFIG_MULTITHREAD=yes
+CONFIG_WEBM_IO=no
+CONFIG_LIBYUV=no
+EOF
+
+  perl build/make/rtcd.pl --arch=generic --sym=vp8_rtcd \
+      --config=rtcd_config.mk vp8/common/rtcd_defs.pl > vp8_rtcd.h
+  perl build/make/rtcd.pl --arch=generic --sym=vp9_rtcd \
+      --config=rtcd_config.mk vp9/common/vp9_rtcd_defs.pl > vp9_rtcd.h
+  perl build/make/rtcd.pl --arch=generic --sym=vpx_dsp_rtcd \
+      --config=rtcd_config.mk vpx_dsp/vpx_dsp_rtcd_defs.pl > vpx_dsp_rtcd.h
+  perl build/make/rtcd.pl --arch=generic --sym=vpx_scale_rtcd \
+      --config=rtcd_config.mk vpx_scale/vpx_scale_rtcd.pl > vpx_scale_rtcd.h
+
+  sed -i 's/#define CONFIG_INSTALL_DOCS[[:space:]]*1/#define CONFIG_INSTALL_DOCS 0/' vpx_config.h
+  sed -i 's/#define CONFIG_POSTPROC[[:space:]]*1/#define CONFIG_POSTPROC 0/' vpx_config.h
+  sed -i 's/#define CONFIG_MULTITHREAD[[:space:]]*0/#define CONFIG_MULTITHREAD 1/' vpx_config.h
+  sed -i 's/#define CONFIG_RUNTIME_CPU_DETECT[[:space:]]*1/#define CONFIG_RUNTIME_CPU_DETECT 0/' vpx_config.h
+
+  cp vpx_config.h vp8_rtcd.h vp9_rtcd.h vpx_dsp_rtcd.h vpx_scale_rtcd.h "$OUTPUT_DIR/"
+  rm -f rtcd_config.mk
+  rm -f vpx_config.h vp8_rtcd.h vp9_rtcd.h vpx_dsp_rtcd.h vpx_scale_rtcd.h
+  exit 0
 fi
 
 sed -i 's/#define HAVE_NEON[[:space:]]*0/#define HAVE_NEON 1/' vpx_config.h
